684bd6654f0894ebb0d0c526c45b6abe3581130d
Leticia0001 <leticia07010@gmail.com>
2025-05-02 12:14:00 +0800

    updatefilename

---
+++ hot-100/ClimbStairs_res.py
@@ -0,0 +1,19 @@
+#Ideas:recursion+memo
+#Time Complexity:O(n)
+#Space complexity:O(n)
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        def dfs(k):
+            if k in memo:
+                return memo[k]
+            if k == 0 or k == 1:
+                return 1
+            memo[k] = dfs(k - 1) + dfs(k - 2)
+            return memo[k]
+        return dfs(n)
+
+
+sol = Solution()
+print(sol.climbStairs(4))  
