263fc656fca7cc1a3bd36fa7fb116cd88cdeb962
Leticia0001 <leticia07010@gmail.com>
2025-05-21 13:29:07 +0800

    update configuration parameters

--- product_of_array_except_self.py
+++ product_of_array_except_self.py
@@ -5,19 +5,19 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        length = len(nums)
-        answer = [1] * length
+        length = len(nums) #make sure the length of the input 
+        output = [1] * length #default output=[1,1,1,1]
 
         # prefix
-        prefix = 1
+        prefix = 1 #default prefix=1 
         for i in range(length):
-            answer[i] = prefix
+            output[i] = prefix
             prefix *= nums[i]
 
         # suffix
-        suffix = 1
-        for i in range(length - 1, -1, -1):
-            answer[i] *= suffix
+        suffix = 1 #default suffix=1
+        for i in range(length - 1, -1, -1): #range(start, stop, step)
+            output[i] *= suffix
             suffix *= nums[i]
 
-        return answer
+        return output
