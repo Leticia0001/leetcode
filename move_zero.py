dab8364da70f32bee1cd767340046f4f947181f8
Leticia0001 <leticia07010@gmail.com>
2025-06-23 19:22:14 +0800

    update move_zero solution

---
+++ move_zero.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        insertPos=0
+        for i in range (len(nums)):
+            if nums[i]!=0:
+                nums[insertPos],nums[i]=nums[i],nums[insertPos] 
+                insertPos=insertPos+1
+
+
+
+        
